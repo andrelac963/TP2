ebc4d4d499db3a3970850608de756ffd074e1868
andrelac963 <andrelac963@gmail.com>
2023-05-26 11:10:49 -0300

    feat: classes graham e jarvis

---
+++ include/graham.hpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------
+// Arquivo      : graham.hpp
+// Conteudo     : programa
+// Autor        : André Luiz Alves Costa (andrelac963@gmail.com)
+// Historico    : 2023-00-00 - arquivo criado
+//---------------------------------------------------------------------
+
+#ifndef GRAHAM_HPP
+#define GRAHAM_HPP
+
+#include <iostream>
+
+using namespace std;
+
+class Graham
+{
+};
+
+#endif
---
+++ include/jarvis.hpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------
+// Arquivo      : jarvis.hpp
+// Conteudo     : programa
+// Autor        : André Luiz Alves Costa (andrelac963@gmail.com)
+// Historico    : 2023-00-00 - arquivo criado
+//---------------------------------------------------------------------
+
+#ifndef JARVIS_HPP
+#define JARVIS_HPP
+
+#include <iostream>
+
+using namespace std;
+
+class Jarvis
+{
+};
+
+#endif
